5ab23a400765c25c784868399c51e01287007f63
ReliableTree <hendrik11ers@msn.com>
2018-04-16 18:59:47 +0200

    2Opt erste Impl, mit und ohne annealing, wie in Print ausgegeben.

--- Opt2.py
+++ Opt2.py
@@ -35,19 +35,20 @@ def opt2(DRIVING_TIMES):
     minTime = compute_total_distance(Route1, DRIVING_TIMES)
     print(minTime, "adv")
     # Route = [1,2,3,4,5,6,7,8,9,10]
-
+    #
     # for perm in it.permutations(Route):
-    #    newRoute = [0] + list(perm) + [0]
-     #   newtime = compute_total_distance(newRoute, DRIVING_TIMES)
-     ##   counter += 1
-      #  if newtime < minTime:
-      #      minTime = newtime
-
-       # if (counter > 100000):
-       #     supcounter += 1
-       #     counter = 0
-
-   # print(minTime, "min")
+    #     newRoute = [0] + list(perm) + [0]
+    #     newtime = compute_total_distance(newRoute, DRIVING_TIMES)
+    #     counter += 1
+    #     if newtime < minTime:
+    #         minTime = newtime
+    #
+    #     if (counter > 100000):
+    #         supcounter += 1
+    #         counter = 0
+    #         print(supcounter)
+    #
+    # print(minTime, "min")
 
 
 
@@ -73,42 +74,56 @@ def opt2Hilf(best_map, i, j):
 
 def start_opt2(road_map, driving_map):
     best_map = road_map[:]
+    ran_map = best_map[:]
     best_distance = compute_total_distance(best_map, driving_map)
-    new_distance = 0
-    new_map = []
-    coolingTemp = 1
-    while coolingTemp >= 0.001:
+    coolingTemp = 1000000
+    new_map = best_map[:]
+    ran_distance = compute_total_distance(new_map, driving_map)
+    for i in range(1, len(best_map)):
+        for j in range(i + 1, len(best_map)):
+            new_map = opt2Hilf(best_map, i, j)
+            new_distance = compute_total_distance(new_map, driving_map)
+
+        if new_distance < ran_distance:
+            ran_distance = new_distance
+            ran_map = new_map[:]
+    print(ran_distance,"best before annealing")
+    while coolingTemp > 100000:
+
+        # ran1 = random.randint(1, len(best_map))
+        # ran2 = random.randint(1, len(best_map))
+        # new_distance = 0
+        #
+        # if ran2 < ran1:
+        #     saveran = 0
+        #     ran1 = saveran
+        #     ran1 = ran2
+        #     ran2 = saveran
+        #
+        # new_map = opt2Hilf(best_map, ran1, ran2)
+
 
         for i in range(1, len(best_map)):
-            for j in range(i+1, len(best_map)):
+            for j in range(i + 1, len(best_map)):
                 new_map = opt2Hilf(best_map, i, j)
                 new_distance = compute_total_distance(new_map, driving_map)
 
-            if new_distance < best_distance:
-                best_distance = new_distance
-                best_map = new_map
+            if new_distance < ran_distance:
+                ran_distance = new_distance
+                ran_map = new_map[:]
 
-        ran1 = random.randint(1, len(best_map))
-        ran2 = random.randint(1, len(best_map))
-        if ran1 < ran2:
-            new_map = opt2Hilf(best_map, ran1, ran2)
-            new_distance = compute_total_distance(new_map, driving_map)
-            print(new_distance, best_distance, new_distance * math.exp(-coolingTemp), "1")
+        ran_distance = compute_total_distance(ran_map, driving_map)
 
-            if (new_distance * math.exp(-coolingTemp)) < best_distance:
-               # print("1>2")
-                best_map = new_map
+        if ((math.exp((best_distance - ran_distance)/coolingTemp)) > random.randrange(0, 1)) and\
+                best_distance != ran_distance:
+            best_map = ran_map[:]
+            best_distance = ran_distance
+            #print(best_distance - ran_distance)
         else:
-            #print(new_distance, best_distance, "2")
-            new_map = opt2Hilf(best_map, ran2, ran1)
-            new_distance = compute_total_distance(new_map, driving_map)
-            if (new_distance * math.exp(-coolingTemp)) < best_distance:
-                #print("2>1")
-                best_map = new_map
-
-        coolingTemp = coolingTemp * 0.999
-
+            coolingTemp -= 1000
     return best_map
+
+
 
 
 def compute_total_distance(Route, driving_map):
