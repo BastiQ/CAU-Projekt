acaac606feed18132b494ef61826344ef82a28e3
reilable_tree <hendrik11ers@msn.com>
2018-04-20 17:58:13 +0200

    cyx

--- solver.py
+++ solver.py
@@ -5,6 +5,7 @@
 ## Underestimated minimum distance ist the sum of the minimum from all rows in the DRIVING_TIMES Matrix
 ## Best algorithm would be 2-Opt + Simulated annealing (i guess)
 import numpy as np
+import time
 import copy
 from Clustering import cluserting
 from Opt2 import opt2
@@ -12,7 +13,7 @@ from Clustering import plot
 np.set_printoptions(threshold=np.nan) # completely print big arrays
 
 # Configure:
-INSTANCE = "Testinstanzen/2_30.txt"
+INSTANCE = "Testinstanzen/1_50.txt"
 VEHICLE_COUNT = 1
 ###################################
 
@@ -41,12 +42,15 @@ with open(INSTANCE) as infile:
     DRIVING_TIMES = np.array(DRIVING_TIMES).astype(np.int32)
 
 # Remove the Exchange points:
-DRIVING_TIMES_SLICED = DRIVING_TIMES[0:-NUM_EXCHANGE, 0:-NUM_EXCHANGE]
+deleteExchangePoints = [p+1 for p in range(NUM_EXCHANGE)]
+DRIVING_TIMES_SLICED = np.delete(DRIVING_TIMES, (deleteExchangePoints), axis=0)
+DRIVING_TIMES_SLICED = np.delete(DRIVING_TIMES_SLICED, (deleteExchangePoints), axis=1)
 nodes = np.shape(DRIVING_TIMES_SLICED)[0]
 
 CLUSTER_COUNT = VEHICLE_COUNT # this changes later
 
 points, clusterSets = cluserting(DRIVING_TIMES_SLICED, CLUSTER_COUNT)
+# plot(points,clusterSets)
 
 paths = []
 for clusterNr, cluster in enumerate(clusterSets):
@@ -55,11 +59,17 @@ for clusterNr, cluster in enumerate(clusterSets):
     B = np.delete(B, (delete), axis=0)
     B = np.delete(B, (delete), axis=1)
 
+    # TODO: labor punkt muss noch mit rein
     print("search ideal path (2-opt)...")
-    path = opt2(B)
+    t0 = time.time()
+    path, timeNeeded = opt2(B, True)
+    t1 = time.time()
+    print(t1 - t0)
     paths.append(path)
-    # print(path)
+    print(path)
 
 plot(points,clusterSets,None,paths)
 
 # VEHICLE_COUNT wird erhoeht von Mutterprogramm (abhaenglig von 2-opt)
+# optimize "startAngleRange" in Clustering, as well as the cluster count.
+# try Angles 90, 120, 180, 220, 260, 300, 340 etc. if 2-opt alg is fast enought
